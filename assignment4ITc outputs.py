cc95060b8d475b561076259349758984ebc3b7f5
jashanjashan <97339122+jashanjashan@users.noreply.github.com>
2022-02-27 20:08:56 +0530

    Add files via upload

---
+++ assignment4ITc outputs.py
@@ -0,0 +1,91 @@
+#q1
+move Disk 1 from A to  B
+move Disk 2 from A to  C
+move Disk 1 from B to  C
+move Disk 3 from A to  B
+move Disk 1 from C to  A
+move Disk 2 from C to  B
+move Disk 1 from A to  B
+
+
+#q2
+using recursion
+enter no: 9
+         
+        1   
+       1   1   
+      1   2   1   
+     1   3   3   1   
+    1   4   6   4   1   
+   1   5   10   10   5   1   
+  1   6   15   20   15   6   1   
+ 1   7   21   35   35   21   7   1   
+1   8   28   56   70   56   28   8   1   
+using iteration
+          1   
+         1   1   
+        1   2   1   
+       1   3   3   1   
+      1   4   6   4   1   
+     1   5   10   10   5   1   
+    1   6   15   20   15   6   1   
+   1   7   21   35   35   21   7   1   
+  1   8   28   56   70   56   28   8   1   
+
+#q3
+a part
+enter a no66
+enter the second no7
+True
+B part
+(9, 3)
+they are none zero
+C part
+(9, 5, 6)
+D part
+{9, 3}
+E part
+frozenset({9, 3})
+F part
+max_value 9
+hash_value -794467754735337211
+
+#q4
+Jashan
+21103087
+now call the del function and destroy object
+object is destroyed
+Traceback (most recent call last):
+  File "C:\Users\dell\Desktop\Documents\assignment4ques4.py", line 15, in <module>
+    print(student_data)
+NameError: name 'student_data' is not defined
+
+
+#q5
+a part
+updated salary of Mehak 70000
+DONE!
+B part
+Traceback (most recent call last):
+  File "C:\Users\dell\Desktop\Documents\assignment4ques5.py", line 19, in <module>
+    print(employee_3,Salary)
+NameError: name 'employee_3' is not defined. Did you mean: 'employee_1'?
+#here erorr indictates the data is deleted
+
+#q6
+word spoken by george: dog
+['dog', 'odg', 'ogd', 'dgo', 'gdo', 'god']
+# the anagram represent words spoken by barbie
+
+
+
+
+
+
+
+
+
+
+
+
+
